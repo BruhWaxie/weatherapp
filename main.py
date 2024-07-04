9886920b3d15bbb7884f436e6e87664a56ffe75c
BruhWaxie <133589196+BruhWaxie@users.noreply.github.com>
2024-07-04 18:32:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,30 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 from kivymd.uix.screen import MDScreen
+from settings import *
+import requests
 
 class HomeScreen(MDScreen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    
+    def get_weather_data(self, url, city):
+        api_params = {
+            'q': city,
+            'appid': API_KEY
+        }
+        data = requests.get(url, api_params)
+        response = data.json()
+        print(response)
+        return response
+    
+
+    def search(self):
+        city = self.ids.city.text.lower().strip()
+        print(city)
+        current_weather = self.get_weather_data(WEATHER_URL, city)
+        temp = current_weather['main']["temp"]
+        self.ids.content_text.text = f'{temp}°C'
 
 class MainApp(MDApp):
     def build(self):
