86ad0194b59ee91b77b8fd6a3b1fd7cd13e37c55
BruhWaxie <133589196+BruhWaxie@users.noreply.github.com>
2024-07-18 18:29:19 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ from kivymd.uix.card import MDCard
 import requests
 
 class WeatherCard(MDCard):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, date, image, temp,desc, temp_like, w_speed,humidity, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.ids.image.source = f'https://openweathermap.org/img/wn/{image}@2x.png'
+        self.ids.date.text = date
+        self.ids.temp = f'{temp}°C'
+        self.ids.temp_like = f'{temp_like}°C'
+        self.ids.desc = desc.capitalize()
+        self.ids.w_speed = f'Швидкість вітру {w_speed} м/с'
+        self.ids.humidity = f'Вологість: {humidity}%'
+
+
+
 
 class HomeScreen(MDScreen):
     def __init__(self, *args, **kwargs):
@@ -23,14 +33,31 @@ class HomeScreen(MDScreen):
         response = data.json()
         print(response)
         return response
-    
+    def create_weather_card(self, data):
+        icon = data['weather'][0]['icon']
+        desc = data['weather'][0]['description']
+        temp = data['main']['temp']
+        temp_like = data['main']['feels_like']
+        humidity = data['main']['humidity']
+        w_speed = data['wind']['speed']
+        if 'dt_text' in data:
+            date = data['dt_text'][5:-3]
+        else:
+            date = 'Зараз'
+        new_card = WeatherCard(data, icon, temp, desc, temp_like, w_speed, humidity)
+        self.ids.weather_carousel.add_widget(new_card)
 
     def search(self):
+        self.ids.weather_carousel.clear_widgets()
         city = self.ids.city.text.lower().strip()
         print(city)
         current_weather = self.get_weather_data(WEATHER_URL, city)
-        temp = current_weather['main']["temp"]
-        self.ids.content_text.text = f'{temp}°C'
+        self.create_weather_card(current_weather)
+
+        forecast = self.get_weather_data(FORECAST_URL, city)
+        for period in forecast['list']:
+            self.create_weather_card(period)
+
 
 class MainApp(MDApp):
     def build(self):
